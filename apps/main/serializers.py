ae41fd0f4c14ce2582c3885f66ff8add1c6fd235
Respect-J <respect0170@gmail.com>
2024-04-29 17:52:38 +0500

    add new models for main

--- apps/main/serializers.py
+++ apps/main/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Banners, Docs, Contacts
+from .models import Banners, Docs, Contacts, TypeDelivery, Advantages, DeliveryFromChina, Directions
 
 
 class BannersSerializer(serializers.ModelSerializer):
@@ -20,3 +20,26 @@ class ContactsSerializer(serializers.ModelSerializer):
         model = Contacts
         fields = ["first_contact", "second_contact"]
 
+
+class DeliveryChinaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DeliveryFromChina
+        fields = ["main_title", "second_title", "description", "img"]
+
+
+class DirectionsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Directions
+        fields = ["main_title", "description", "img"]
+
+
+class AdvantagesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Advantages
+        fields = ["main_title", "second_title", "description", "img"]
+
+
+class TypeDeliverySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TypeDelivery
+        fields = ["main_title", "description", "img"]
--- apps/main/views.py
+++ apps/main/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics
-from .models import Banners, Docs, Contacts
-from .serializers import BannersSerializer, DocsSerializer, ContactsSerializer
+from .models import Banners, Docs, Contacts, TypeDelivery, Directions, DeliveryFromChina, Advantages
+from .serializers import BannersSerializer, DocsSerializer, ContactsSerializer, TypeDeliverySerializer, \
+    DirectionsSerializer, DeliveryChinaSerializer, AdvantagesSerializer
 
 
 class BannersView(generics.ListAPIView):
@@ -16,3 +17,23 @@ class DocsView(generics.ListAPIView):
 class ContactsView(generics.ListAPIView):
     queryset = Contacts.objects.all()
     serializer_class = ContactsSerializer
+
+
+class TypeDeliveryView(generics.ListAPIView):
+    queryset = TypeDelivery.objects.all()
+    serializer_class = TypeDeliverySerializer
+
+
+class DirectionsView(generics.ListAPIView):
+    queryset = Directions.objects.all()
+    serializer_class = DirectionsSerializer
+
+
+class DeliveryFromChinaView(generics.ListAPIView):
+    queryset = DeliveryFromChina.objects.all()
+    serializer_class = DeliveryChinaSerializer
+
+
+class AdvantagesView(generics.ListAPIView):
+    queryset = Advantages.objects.all()
+    serializer_class = AdvantagesSerializer
