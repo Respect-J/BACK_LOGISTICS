9709b8acf05ecab589bf0b26fd6902942a481a9e
Respect-J <respect0170@gmail.com>
2024-08-11 15:04:13 +0500

    hello world

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,5 +1,13 @@
+import random
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+
+
+def generate_unique_id():
+    while True:
+        unique_id = random.randint(1000000, 9999999)
+        if not CustomUser.objects.filter(id=unique_id).exists():
+            return unique_id
 
 
 class CustomUser(AbstractUser):
@@ -9,6 +17,16 @@ class CustomUser(AbstractUser):
         ('sales', 'Sales Manager'),
     )
     user_type = models.CharField(max_length=10, choices=USER_TYPE_CHOICES, default='regular')
+    id = models.IntegerField(primary_key=True, default=generate_unique_id, editable=False)
 
     def __str__(self):
         return self.username
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.id = generate_unique_id()
+        super().save(*args, **kwargs)
+
+    def clean(self):
+        if not (1000000 <= self.id <= 9999999):
+            raise ValidationError('ID должен быть 7-значным числом.')
