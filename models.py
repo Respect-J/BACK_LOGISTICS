5d46a434167af3a2633d39eba3258bf7e7560df0
Respect-J <respect0170@gmail.com>
2024-08-11 15:57:09 +0500

    hello world

--- models.py
+++ models.py
@@ -8,3 +8,6 @@ class BaseModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        abstract = True
+
