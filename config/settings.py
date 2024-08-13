9c73d54b5a709f545b0c1870ecc80eaff0876736
Respect-J <respect0170@gmail.com>
2024-08-13 20:55:08 +0500

    hello world

--- config/settings.py
+++ config/settings.py
@@ -162,5 +162,5 @@ STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_URL = "media/"
-MEDIA_ROOT = "/home/legacyex/back-legacy-ex.com/media/"
+MEDIA_ROOT = "/home/legacyex/back.legacy-ex.com/media/"
 
