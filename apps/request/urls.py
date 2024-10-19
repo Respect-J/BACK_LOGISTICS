8947453d6aaabfae03d049f76ec086644d873b5b
Little <146277525+yusuf-gh@users.noreply.github.com>
2024-10-19 16:38:17 +0500

    Update urls.py

--- apps/request/urls.py
+++ apps/request/urls.py
@@ -5,9 +5,9 @@ urlpatterns = [
 
     path('request_filter_post/', 
          RequestDetailView.as_view({'post': 'create', 'get': 'list'}), 
-         name='request-filter_post'), 
+         name='request-filter_post'), #фильтрации по датам и статусу + POST обработка
 
     path('requests/<uuid:pk>/', 
          RequestDetailView.as_view({'get': 'retrieve', 'put': 'update', 'patch': 'partial_update', 'delete': 'destroy'}), 
-         name='request-detail')
+         name='request-detail')  # методы GET, PUT, PATCH, DELETE с указанием ID
 ]
