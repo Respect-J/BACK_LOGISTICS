db029ce7f5b2f9236f8f6b1a545bb3fd79248b33
Respect-J <79748419+Respect-J@users.noreply.github.com>
2024-11-29 14:38:22 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -150,9 +150,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'respect0170@gmail.com'
-EMAIL_HOST_PASSWORD = 'respect707'
-DEFAULT_FROM_EMAIL = 'respect0170@gmail.com'
+EMAIL_HOST_USER = '@gmail.com'
+EMAIL_HOST_PASSWORD = ''
+DEFAULT_FROM_EMAIL = '@gmail.com'
 
 
 # Password validation
